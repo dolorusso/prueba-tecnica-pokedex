a47009b747f21ad18235dcc978cf7156fd69080f
Dolores Russo <158060083+dolorusso@users.noreply.github.com>
2024-10-30 11:52:37 -0300

    Agrego los tipos (#4)
    * carta del pokemon con sus estilos
    
    * detalles del pokemon al ver mas
    
    * tipos de pokemon

---
+++ src/components/typeStyles.js
@@ -0,0 +1,38 @@
+import { 
+  FaLeaf, 
+  FaFire, 
+  FaTint, 
+  FaBolt, 
+  FaBug, 
+  FaDragon, 
+  FaFish, 
+  FaHippo, 
+  FaPaw, 
+  FaMountain, 
+  FaSnowflake, 
+  FaCloudSun, 
+  FaWater, 
+  FaSpider, 
+  FaGhost  
+} from 'react-icons/fa';
+
+export const typeStyles = {
+  normal: { color: '#A8A77A', icon: <FaPaw /> },
+  fighting: { color: '#C22E28', icon: <FaDragon /> },
+  flying: { color: '#A98FF3', icon: <FaCloudSun /> },
+  poison: { color: '#A33EA1', icon: <FaSpider /> },
+  ground: { color: '#E2BF68', icon: <FaMountain /> },
+  rock: { color: '#B6A136', icon: <FaMountain /> },
+  bug: { color: '#A8B820', icon: <FaBug /> },
+  ghost: { color: '#705898', icon: <FaGhost /> }, 
+  steel: { color: '#B7B7CE', icon: <FaMountain /> },
+  fire: { color: '#F08030', icon: <FaFire /> },
+  water: { color: '#6890F0', icon: <FaTint /> },
+  grass: { color: '#78C850', icon: <FaLeaf /> },
+  electric: { color: '#F8D030', icon: <FaBolt /> },
+  psychic: { color: '#F85888', icon: <FaFish /> },
+  ice: { color: '#98D8D8', icon: <FaSnowflake /> },
+  dragon: { color: '#6F35FC', icon: <FaDragon /> },
+  dark: { color: '#705848', icon: <FaDragon /> },
+  fairy: { color: '#D685AD', icon: <FaFire /> },
+};
