c3f56fd59efbde733379c160d019472c3cb17653
Dolores Russo <drusso@alumnos.exa.unicen.edu.ar>
2024-10-30 10:47:08 -0300

    commit incial de config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com',
+     'e7.pngegg.com'],},
 };
 
 export default nextConfig;
